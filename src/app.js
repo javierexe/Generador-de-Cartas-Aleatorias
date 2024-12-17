b64490e7af01413f3a8c6707aa0e4a804b028870
javierexe <javier.exe.valenzuela@gmail.com>
2024-12-16 21:50:31 -0300

    proyecto listo para entregar

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pintas = ["♦", "♥", "♠", "♣"];
+  let cartas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+  let pinta = Math.floor(Math.random() * pintas.length);
+  let carta = Math.floor(Math.random() * cartas.length);
+
+  let cardHeader = document.getElementsByClassName("pintaArriba");
+  let cardfooter = document.getElementsByClassName("pintaAbajo");
+  let cardNumber = document.getElementsByClassName("numeroCarta");
+
+  pintaArriba.innerHTML = pintas[pinta];
+  pintaAbajo.innerHTML = pintas[pinta];
+  numeroCarta.innerHTML = cartas[carta];
+
+  if (pintas[pinta] == "♠" || pintas[pinta] == "♣") {
+    document.getElementById("carta").style.color = "black";
+  } else {
+    document.getElementById("carta").style.color = "red";
+  }
 };
